2ac46e6a1b062186c9f15be1f08d69ee09de5189
Doug Russell <doug@getitdownonpaper.com>
2018-02-06 20:43:43 -0500

    move makeSystemApplicationController outside protocol

--- AccessibilityElement/Bundle.swift
+++ AccessibilityElement/Bundle.swift
@@ -6,20 +6,18 @@
 
 import Foundation
 
+public func makeSystemApplicationController(processIdentifier: Int) throws -> AnyController {
+    let uiElement = AXUIElement.application(processIdentifier: processIdentifier)
+    let element = Element(element: uiElement)
+    let node = DefaultHierarchy<Element>().buildHierarchy(from: element)
+    let eventHandler: AnyEventHandler?
+    let observerManager = ObserverManager(provider: SystemObserverProviding.provider())
+    eventHandler = try? EventHandlerRegistrar.shared.eventHandler(node: node, observerManager: observerManager)
+    return try eventHandler?.makeController() as! _Controller<Element>
+}
+
 public protocol AccessibilityBundle : class {
     // Return controller representing the application
     func load(processIdentifier: Int) throws -> AnyController
     init()
-}
-
-public extension AccessibilityBundle {
-    public func makeSystemApplicationController(processIdentifier: Int) throws -> AnyController {
-        let uiElement = AXUIElement.application(processIdentifier: processIdentifier)
-        let element = Element(element: uiElement)
-        let node = DefaultHierarchy<Element>().buildHierarchy(from: element)
-        let eventHandler: AnyEventHandler?
-        let observerManager = ObserverManager(provider: SystemObserverProviding.provider())
-        eventHandler = try? EventHandlerRegistrar.shared.eventHandler(node: node, observerManager: observerManager)
-        return try eventHandler?.makeController() as! _Controller<Element>
-    }
 }
