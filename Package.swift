2c70b4f97a5a7ca21fb840e53581bb12ee781744
Doug Russell <doug@getitdownonpaper.com>
2018-03-20 12:23:21 -0400

    Bump CFOverlay

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["AccessibilityElement"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/rustle/CoreFoundationOverlay.git", from: "1.0.0"),
+        .package(url: "https://github.com/rustle/CoreFoundationOverlay.git", from: "1.0.1"),
         .package(url: "https://github.com/rustle/Signals.git", .revision("37445e0039defc57f8914ed521d2c1a0772fe992")),
         .package(url: "https://github.com/rustle/SwiftScanner.git", from: "1.0.3+"),
         .package(url: "https://github.com/glessard/swift-atomics", from: "4.0.0"),
