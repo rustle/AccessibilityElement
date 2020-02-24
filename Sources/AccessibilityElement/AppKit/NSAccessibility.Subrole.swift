16d3e6ee27c7111a9dc87bc1fa8893b194319c48
Doug Russell <doug@getitdownonpaper.com>
2020-02-23 21:54:06 -0500

    Let NSAccessibility.Subrole Codable conformance be generated by compiler

--- Sources/AccessibilityElement/AppKit/NSAccessibility.Subrole.swift
+++ Sources/AccessibilityElement/AppKit/NSAccessibility.Subrole.swift
@@ -6,16 +6,4 @@
 
 import Cocoa
 
-extension NSAccessibility.Subrole: Codable {
-    public enum CodingKeys: String, CodingKey {
-        case rawValue
-    }
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(rawValue, forKey: .rawValue)
-    }
-    public init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        self.init(rawValue: try values.decode(String.self, forKey: .rawValue))
-    }
-}
+extension NSAccessibility.Subrole: Codable {}
