7f2c3f7d10f087e90917a8403ec8860a74597c33
Doug Russell <doug@getitdownonpaper.com>
2020-02-23 21:52:41 -0500

    Let NSAccessibility.Role Codable conformance be generated by compiler

--- Sources/AccessibilityElement/AppKit/NSAccessibility.Role.swift
+++ Sources/AccessibilityElement/AppKit/NSAccessibility.Role.swift
@@ -11,16 +11,4 @@ public extension NSAccessibility.Role {
     static let webArea = NSAccessibility.Role(rawValue: "AXWebArea")
 }
 
-extension NSAccessibility.Role: Codable {
-    public enum CodingKeys: String, CodingKey {
-        case rawValue
-    }
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(rawValue, forKey: .rawValue)
-    }
-    public init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        self.init(rawValue: try values.decode(String.self, forKey: .rawValue))
-    }
-}
+extension NSAccessibility.Role: Codable {}
