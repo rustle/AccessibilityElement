edb394a60ed4dd86ce098a279868a3787f4417e7
Doug Russell <doug@getitdownonpaper.com>
2026-04-15 23:11:03 -0400

    Bump AX Version

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/rustle/AX.git",
-            .upToNextMajor(from: "0.2.0")
+            .upToNextMajor(from: "0.2.1")
         ),
         .package(
             url: "https://github.com/apple/swift-atomics.git",
